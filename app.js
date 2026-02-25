d4e672588fc1bbc12db00535f4167581edeee168
imakshaywankhede-beep <imakshaywankhede@gmail.com>
2026-02-25 18:36:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
- res.send("Hello devops🚀");
+ res.send("Hello devops🚀 self-hosted");
 });
 
 app.listen(3000, () => {
